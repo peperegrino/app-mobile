950cf9d30df29d854beb4ffd4e12544791249c3d
LUIZ FELIPE ALMEIDA LIMA <luiz.lima60@senaisp.edu.br>
2026-04-02 11:01:46 -0300

    teste de projetos js

---
+++ assets/components/teskCard.jsx
@@ -0,0 +1,8 @@
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+
+export default function taskCard ({item, onDelete, onEdit}) {
+    const priorityColor = TouchableNativeFeedbackComponent.colors.priority
+    [item.priority] || TouchableNativeFeedbackComponent.colors.textSub;
+
+    
+}
