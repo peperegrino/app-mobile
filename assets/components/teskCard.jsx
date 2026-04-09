341ff3154b3db0e4fcb74d1d1cf5a4d9da254d0f
peperegrino <luizfalmeidalima38@gmail.com>
2026-04-09 10:35:29 -0300

    criei os componentes card e taskmodal, estilizei ambos

--- assets/components/teskCard.jsx
+++
@@ -1,8 +0,0 @@
-import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
-
-export default function taskCard ({item, onDelete, onEdit}) {
-    const priorityColor = TouchableNativeFeedbackComponent.colors.priority
-    [item.priority] || TouchableNativeFeedbackComponent.colors.textSub;
-
-    
-}
